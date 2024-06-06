66bf731bc985610c9199c957652e3df59f679645
Marthin Lachira <marthin.lachira@mail.com>
2024-06-06 11:47:37 -0500

    feat: devops vercel v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+
+let adapter = vercel();
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
   output: "server",
+  adapter: adapter(),
 });
-
